4648e2f8e3c68053ebd96af598b629ef03dc0177
imaninaitore <imaninaitore1726@gmail.com>
2026-07-15 12:07:47 +0300

    feat: wrapp app in browser router and import route in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import StudyLevel from './pages/StudyLevel'
+import { Route,Routes } from 'react-router'
 
 function App() {
   return (
     <>
     <Navbar/>
     <Header/>
+
+    <Routes>
+       <Route path="/StudyLevel" element={<StudyLevel />} />
+
+    </Routes>
     </>
 
 
