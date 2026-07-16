75b343560d8fa371ea058241e4ec811f8ba20f3f
imaninaitore <imaninaitore1726@gmail.com>
2026-07-16 09:14:55 +0300

    feat:correct navigation path to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
     <Routes>
-       <Route path="/home" element={
+       <Route path="/" element={
           <>
             <Navbar />
             <Header />
