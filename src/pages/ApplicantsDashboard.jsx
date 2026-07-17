60562aae205d82a5e8c9d7842e71e7ccd5a55c44
imaninaitore <imaninaitore1726@gmail.com>
2026-07-17 23:48:56 +0300

    feat: get currentuser and application

--- src/pages/ApplicantsDashboard.jsx
+++ src/pages/ApplicantsDashboard.jsx
@@ -2,3 +2,11 @@ import { Link } from "react-router";
 import { User, GraduationCap, FileText, Clock } from "lucide-react";
 import { useAuth } from "@/context/AuthContext";
 import { useApplications } from "@/context/ApplicationContext";
+
+const ApplicantDashboard = () => {
+  const { currentUser } = useAuth();
+  const { application } = useApplications();
+  return(
+
+  );
+};
