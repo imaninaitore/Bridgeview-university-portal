f11823394192b19fe9280c9521132225b67437d6
imaninaitore <imaninaitore1726@gmail.com>
2026-07-17 17:26:47 +0300

    feat: import the context

--- src/components/Progammes.jsx
+++ src/components/Progammes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useProgrammes } from '@/context/ProgrammeContext'
 function Progammes() {
+    const { programmes, loading} = useProgrammes();
   return (
     <div>Progammes</div>
   )
